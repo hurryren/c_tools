bdb3e6b99781403ac3a9b9f273162462564958f1
orange <orange@format.com>
2021-02-25 21:18:32 +0800

    update xv6

--- pro/xv6/x86.h
+++ pro/xv6/x86.h
@@ -106,6 +106,50 @@ static inline uint rcr2(void){
 }
 
 
+static inline void lcr3(uint val){
+    asm volatile("movl %0,%%cr3" :: "r" (val));
+}
+
+
+// layout  of the trap frame built on the stack by the hardware
+// and by strapasm.S, and passd to trap()
+struct trapframe{
+    // registers as pushed by pusha
+    uint edi;
+    uint esi;
+    uint ebp;
+    uint oesp;  // useless & ignored
+    uint ebx;
+    uint edx;
+    uint ecx;
+    uint eax;
+
+    // rest of trap frame
+    ushort gs;
+    ushort padding1;
+    ushort fs;
+    ushort padding2;
+    ushort es;
+    ushort padding3;
+    ushort ds;
+    ushort padding4;
+    uint trapno;
+
+    // below here defined by x86 hardware
+    uint err;
+    uint eip;
+    ushort cs;
+    ushort padding5;
+    uint eflags;
+
+    // below here only when crossing rings, such as from user ot kernel
+    uint esp;
+    ushort ss;
+    ushort padding6;
+};
+
+
+
 
 
 
