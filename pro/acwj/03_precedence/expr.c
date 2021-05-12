6c6379ae46e12d2910bbcea20c335268447f534d
orange <823renj@gmail.com>
2021-05-12 16:41:34 +0800

    update

---
+++ pro/acwj/03_precedence/expr.c
@@ -0,0 +1,96 @@
+#include "defs.h"
+#include "data.h"
+#include "decl.h"
+
+
+// Parse a primary factor and return an
+ // Ast node representing it
+ static struct ASTnode *primary(void){
+     struct ASTnode *n;
+
+     // for an INTLIT token, make a leaf AST node for it
+     // scan in the next token. other wise, a syntax error
+     // for any other token type
+     switch(Token.token){
+         case T_INTLIT:
+            n = mkastleaf(A_INTLIT,Token.intvalue);
+            scan(&Token);
+            return n;
+
+        default:
+            fprintf(stderr, "syntax error on line %d\n", Line);
+            exit(1);
+     }
+ }
+
+
+// convert a token into AST operation
+int arithop(int tok){
+    switch(tok){
+        case T_PLUS:
+            return (A_ADD);
+        case T_MINUS:
+            return (A_SUBTRACT);
+        case T_STAR:
+            return (A_MULTIPLY);
+        case T_SLASH:
+            return (A_DIVIDA);
+        default:
+            fprintf(stderr, "unkown token in arithop() on line %d\n", Line);
+            exit(1);
+    }
+}
+
+// opeartor precedence for each token
+static int OpPrec[] = {0, 10, 10,20,20,0};
+
+// check that we have a binary operator and return its precedence
+static int op_precedence(int tokentype){
+    int prec = OpPrec[tokentype];
+    if(prec == 0){
+        fprintf(stderr, "syntax error on line %d, token %d\n",Line,tokentype);
+        exit(1);
+    }
+    return prec;
+}
+
+// return an AST tree whose root is a binary operator
+// paarameter ptp is the previous token's precedence
+struct ASTnode *binexpr(int ptp){
+    struct ASTnode *left, *right;
+    int tokentype;
+
+    // get the integer literal one the left
+    // fetch the next token at the same time
+    left = primary();
+
+    // if no tokens left, return just the left node
+    tokentype = Token.token;
+    if(tokentype == T_EOF)
+        return left;
+
+    // Whilw the precedence of this token is \
+    // more than that of the previous token precedence
+    while(op_precedence(tokentype) > ptp){
+        // fetch in the next integer literal
+        scan(&Token);
+
+        // recursively call binexpr() with the
+        // precedence of our token to build a sub-tree
+        right = binexpr(OpPrec[tokentype]);
+
+        // join that sub-tree with ours, Convert the token
+        // into an AST operation at the same time
+        left = mkastnode(arithop(tokentype),left,right,0);
+
+        // update the details of the current token.
+        // if no tokens left, return just the left node
+        tokentype = Token.token;
+        if(tokentype == T_EOF)
+            return left;
+    }
+
+    // return the tree we have wehn the precedence is the same or lower
+    return left;
+}
+
---
+++ pro/acwj/03_precedence/main.c
@@ -0,0 +1,45 @@
+#include "defs.h"
+#define extern_
+#include "data.h"
+#undef extern_
+#include "decl.h"
+#include <errno.h>
+
+
+// compiler setup and top-level execution
+
+// initialise global variables
+static void init(){
+    Line = 1;
+    Putback = '\n';
+}
+
+// print out a usage if started incorrectly
+static void usage(char *prog){
+    fprintf(stderr,"Usage: %s infile\n",prog);
+    exit(1);
+}
+
+// list of printable tokens
+char *tokstr[] = {"+","-","*","/","intlit"};
+
+// main program: check arguments and print a usage
+// if we don't have an argument. Open up the input
+// file and call scanfile() to scan the tokens in it
+void main(int argc, char *argv[]){
+    struct ASTnode *n;
+
+    if(argc != 2)
+        usage(argv[0]);
+
+    init();
+
+    if((Infile = fopen(argv[1],"r")) == NULL){
+        fprintf(stderr, "Unable to open %s: %s\n", argv[1],strerror(errno));
+        exit(1);
+    }
+    scan(&Token);   // get the first token from the input
+    n = binexpr(0);  // Parse the expression in the file
+    printf("%d\n",interpretAST(n));     // Calculate the final result
+    exit(0);
+}
